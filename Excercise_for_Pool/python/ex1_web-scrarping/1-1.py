c59f001a7b8f0eef98a255a6b3f376e5fedef288
sawawada <b224346@hiroshima-u.ac.jp>
2023-10-20 15:32:41 +0900

    first_commit

---
+++ Excercise_for_Pool/python/ex1_web-scrarping/1-1.py
@@ -0,0 +1,81 @@
+#使用するモジュール
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+#グルナビのurl
+url="https://r.gnavi.co.jp/"
+res = requests.get(url)
+soup=BeautifulSoup(res.text,'html.parser')
+url_list=[]
+for x in range(1,4):
+    url_list.append(f'https://r.gnavi.co.jp/area/jp/rs/?p={x}')
+    
+Big_list=[]
+import time
+ # 正規表現を使用して住所を分割する関数
+def split_address(address):
+           
+            pattern = r'(.+?[都道府県|都|道|府|県])(.+?)([\d\-]+)'
+            match = re.match(pattern, address)
+            if match:
+                prefecture = match.group(1).strip()
+                city = match.group(2).strip()
+                block = match.group(3).strip()
+                return prefecture, city, block
+            else:
+                return None, None, None           
+#1-3ページでURLを取得し、それぞれの店舗情報を取り出す、
+for x in url_list:
+    url=x
+    time.sleep(3)
+    res = requests.get(url)
+    soup=BeautifulSoup(res.text,'html.parser')
+    restaurant_list=soup.find('div',class_="style_resultRestaurant__WhVwP")
+    a_tag2=restaurant_list.find_all('a',class_="style_titleLink__oiHVJ")
+    restaurant_link_list=[x['href']for x in a_tag2]
+#urlを１つずつ調べる    
+    for y in restaurant_link_list:
+        url=y
+        res= requests.get(url)
+        res.encoding = res.apparent_encoding
+        soup=BeautifulSoup(res.text,'html.parser')
+        #店舗名
+        name=soup.find('p',class_="fn org summary").string
+        #電話番号
+        phone_number=soup.find('span', class_="number").string
+        #メール
+        mail=soup.find('a', href=True, text='お問い合わせ')
+        if mail is None:
+            mail = ''
+        #住所を取得
+        address=soup.find('span', class_="region").string
+        #住所を分割
+        prefecture, city, block= split_address(address)
+        if prefecture is None:
+            prefecture = ''
+        if city is None:
+            city = ''
+        if block is None:
+            block = ''
+        #建物名    
+        building_name_tag = soup.find('span', class_="locality")
+        building_name = building_name_tag.string if building_name_tag else ''
+        #ssl
+        is_ssl = url.startswith('https')
+
+        if is_ssl:
+            SSL='TRUE'
+        else:
+            SSL='FALSE'
+        #店の情報
+        shop_list=[name,phone_number,mail,prefecture,city,block,building_name,url,SSL]
+        #店の情報のリスト
+        Big_list.append(shop_list)
+#データフレイム
+df=pd.DataFrame(Big_list,columns=['name','phone_number','mail','prefecture','city','block','building_name','url','SSL'])
+
+df_50=df.head(50)
+#csvfile 
+csv_filename = '1-1.csv'  # 保存するCSVファイル名
+df_50.to_csv(csv_filename, encoding='utf-8-sig', index=False,sep=',')  # index=Falseでインデックスを保存しない
---
+++ Excercise_for_Pool/python/ex1_web-scrarping/1-2.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(4)
+
+
+#店舗検索一覧の１ページを開く
+page1='https://r.gnavi.co.jp/area/jp/rs/'
+driver.get(page1)
+
+
+# 住所を都道府県、市区町村、番地に分割する関数
+def split_address(address):
+        
+    pattern = r'(.+?[都道府県|都|道|府|県])(.+?)([\d\-]+)'
+    match = re.match(pattern, address)
+    if match:
+        prefecture = match.group(1).strip()
+        city = match.group(2).strip()
+        block = match.group(3).strip()
+        return prefecture, city, block
+    else:
+        return None, None, None
+#店の情報のリストを格納するリスト   
+BIG_LIST=[]   
+for p in range(0,3):
+    # 現在のページのURLを取得
+    
+    page = driver.current_url
+    print("Current Page:", page)  # 現在のページのURLを表示
+    s= driver.find_elements(By.XPATH, '//div[@class="style_title___HrjW"]/a')
+    
+    for i in range(len(s)):
+        # 再度要素を取得
+        s = driver.find_elements(By.XPATH, '//div[@class="style_title___HrjW"]/a')
+        element = s[i]
+        element.click()
+        #「店舗名」
+        name=driver.find_element(By.ID,'info-name').text
+        #「電話番号」
+        phone_number=driver.find_element(By.CLASS_NAME,'number').text
+        #「メールアドレス」
+        mail=''
+        #アドレス所得
+        address=driver.find_element(By.CLASS_NAME,'region').text
+        #「都道府県」#「市区町村」#「番地」
+    
+    
+        prefecture, city, block= split_address(address)
+        if prefecture is None:
+            prefecture = ''
+        if city is None:
+            city = ''
+        if block is None:
+            block = ''
+
+        #「建物名」
+        try:
+            building_name = driver.find_element(By.CLASS_NAME, 'locality').text
+        except:
+            building_name = None
+        #「URL」
+        URL=driver.current_url
+        #「SSL」
+        is_ssl = URL.startswith('https')
+
+        if is_ssl:
+            SSL='TRUE'
+        else:
+            SSL='FALSE'
+        store_infomations=[name,phone_number,mail,prefecture,city,block,building_name,URL,SSL]
+        BIG_LIST.append(store_infomations)
+        #店舗ページに戻る
+        back_botton=driver.find_element(By.ID,"gn_info-breadcrumbs-htpback-go")
+        back_botton.click()
+    #次のページに行くボタン    
+    next_button = driver.find_element(By.CLASS_NAME,"style_nextIcon__M_Me_")
+    next_button.click()  
+#データフレームに      
+df=pd.DataFrame(BIG_LIST,columns=['name','phone_number','mail','prefecture','city','block','building_name','url','SSL'])
+df_50=df.head(50)  
+csv_filename='1-2.csv'
+df_50.to_csv(csv_filename, encoding='utf-8-sig', index=False,sep=',')  # index=Falseでインデックスを保存しない
