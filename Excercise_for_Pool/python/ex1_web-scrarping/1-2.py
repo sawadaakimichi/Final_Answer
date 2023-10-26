e673cad100469d72f7527306b52fdbf36a8b7634
sawawada <b224346@hiroshima-u.ac.jp>
2023-10-26 18:22:21 +0900

    second-commit

--- Excercise_for_Pool/python/ex1_web-scrarping/1-2.py
+++ Excercise_for_Pool/python/ex1_web-scrarping/1-2.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time  
 
 # ユーザーエージェントを指定
 options = Options()
@@ -15,18 +16,16 @@ options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.
 # Chromeドライバーを作成
 driver = webdriver.Chrome(options=options)
 
-
 driver.implicitly_wait(10)
 
+# 店舗検索一覧の１ページを開く
 
-#店舗検索一覧の１ページを開く
-page1='https://r.gnavi.co.jp/area/jp/rs/'
+page1 = 'https://r.gnavi.co.jp/area/jp/rs/'
 driver.get(page1)
 
-
 # 住所を都道府県、市区町村、番地に分割する関数
+
 def split_address(address):
-        
     pattern = r'(.+?[都道府県|都|道|府|県])(.+?)([\d\-]+)'
     match = re.match(pattern, address)
     if match:
@@ -36,32 +35,39 @@ def split_address(address):
         return prefecture, city, block
     else:
         return None, None, None
-#店の情報のリストを格納するリスト   
-BIG_LIST=[]   
-for p in range(0,3):
+
+# 店の情報のリストを格納するリスト
+
+BIG_LIST = []
+
+# 1回目の3秒待機
+time.sleep(3)
+
+for p in range(0, 3):
+
     # 現在のページのURLを取得
-    
+
     page = driver.current_url
     print("Current Page:", page)  # 現在のページのURLを表示
-    s= driver.find_elements(By.XPATH, '//div[@class="style_title___HrjW"]/a')
-    
+
+    s = driver.find_elements(By.XPATH, '//div[@class="style_title___HrjW"]/a')
+
     for i in range(len(s)):
         # 再度要素を取得
         s = driver.find_elements(By.XPATH, '//div[@class="style_title___HrjW"]/a')
         element = s[i]
         element.click()
         #「店舗名」
-        name=driver.find_element(By.ID,'info-name').text
+        name = driver.find_element(By.ID, 'info-name').text
         #「電話番号」
-        phone_number=driver.find_element(By.CLASS_NAME,'number').text
+        phone_number = driver.find_element(By.CLASS_NAME, 'number').text
         #「メールアドレス」
-        mail=''
-        #アドレス所得
-        address=driver.find_element(By.CLASS_NAME,'region').text
+        mail = ''
+        # アドレス所得
+        address = driver.find_element(By.CLASS_NAME, 'region').text
         #「都道府県」#「市区町村」#「番地」
-    
-    
-        prefecture, city, block= split_address(address)
+
+        prefecture, city, block = split_address(address)
         if prefecture is None:
             prefecture = ''
         if city is None:
@@ -75,25 +81,31 @@ for p in range(0,3):
         except:
             building_name = None
         #「URL」
-        URL=driver.current_url
+        URL = driver.current_url
         #「SSL」
         is_ssl = URL.startswith('https')
 
         if is_ssl:
-            SSL='TRUE'
+            SSL = 'TRUE'
         else:
-            SSL='FALSE'
-        store_infomations=[name,phone_number,mail,prefecture,city,block,building_name,URL,SSL]
+            SSL = 'FALSE'
+        store_infomations = [name, phone_number, mail, prefecture, city, block, building_name, URL, SSL]
         BIG_LIST.append(store_infomations)
         #店舗ページに戻る
-        back_botton=driver.find_element(By.ID,"gn_info-breadcrumbs-htpback-go")
+        back_botton = driver.find_element(By.ID, "gn_info-breadcrumbs-htpback-go")
         back_botton.click()
-    #次のページに行くボタン    
-    next_button = driver.find_element(By.CLASS_NAME,"style_nextIcon__M_Me_")
-    next_button.click()  
+
+    # 次のページに行くボタン
+    next_button = driver.find_element(By.CLASS_NAME, "style_nextIcon__M_Me_")
+    next_button.click()
+
+    # 次のページに移動する前に3秒待機
+    time.sleep(3)
+
 driver.quit()
-#データフレームに      
-df=pd.DataFrame(BIG_LIST,columns=['店舗名','電話番号','メールアドレス','都道府県','市区町村','番地','建物名','URL','SSL'])
-df_50=df.head(50)  
-csv_filename='1-2.csv'
-df_50.to_csv(csv_filename, encoding='utf-8-sig', index=False,sep=',')  # index=Falseでインデックスを保存しない
+
+# データフレームに
+df = pd.DataFrame(BIG_LIST, columns=['店舗名', '電話番号', 'メールアドレス', '都道府県', '市区町村', '番地', '建物名', 'URL', 'SSL'])
+df_50 = df.head(50)
+csv_filename = '1-2.csv'
+df_50.to_csv(csv_filename, encoding='utf-8-sig', index=False, sep=',')  # index=Falseでインデックスを保存しない
